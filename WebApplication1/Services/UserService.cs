012fe22cbe801f164973318695da873d333a1cdd
MateuszMarter <mateuszmarter@gmail.com>
2024-06-28 10:05:28 +0200

    final

--- WebApplication1/Services/UserService.cs
+++ WebApplication1/Services/UserService.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Context;
 using WebApplication1.DTO;
+using WebApplication1.Models;
 
 namespace WebApplication1.Services;
 
@@ -27,10 +28,19 @@ public class UserService : IUserService
 
     public async Task<object> GetCommitments(int idUser, string orderBy)
     {
-        List<UserCommitmentsDTO> userCommitments = new List<UserCommitmentsDTO>();
-        
-        
+        var userCommitments = _context.Users
+            .Join(_context.Subscriptions
+                , u => u.IdUser
+                , s => s.IdUser
+                , (u, s) => u.IdUser == s.IdUser);
+            //.Join(_context.Commitments, c => c);
 
+        /*if (orderBy.Equals("PaymentDeadline"))
+            await userCommitments.OrderBy(c => c.PaymentDeadline).ToListAsync();
+        if(orderBy.Equals("LeftToPay"))
+            await userCommitments.OrderBy(c => c.LeftToPay).ToListAsync();*/
+        
+        
         return userCommitments;
     }
 }
